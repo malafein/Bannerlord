1973425267474f5bd97dc9fc21ff68167e55fb1c
malafein <kjmathis@gmail.com>
2020-04-12 11:14:30 -0500

    Basic functionality of Friendly and Threatening missives.

--- CalradianPostalService/Models/MissiveThreat.cs
+++ CalradianPostalService/Models/MissiveThreat.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.Core;
 using TaleWorlds.SaveSystem;
 
@@ -20,6 +21,22 @@ namespace CalradianPostalService.Models
         public override void OnDelivery()
         {
             CPSModule.DebugMessage("OnDelivery called.", log);
+            float relationWithSender = (float)Recipient.GetRelation(Sender);
+            float roll = MBRandom.RandomFloat;
+            CPSModule.DebugMessage($"relationWithSender: {relationWithSender}, roll: {roll}", log);
+            if (roll <= (relationWithSender + 100.0f) / 100.0f)
+            {
+                if (Hero.MainHero == Sender)
+                {
+                    CPSModule.InfoMessage($"{Recipient} was angered your letter.");
+                }
+
+                ChangeRelationAction.ApplyRelationChangeBetweenHeroes(Sender, Recipient, -1);
+            }
+            else if (Hero.MainHero == Sender)
+            {
+                CPSModule.InfoMessage($"{Recipient} received your letter, but was not impressed.");
+            }
         }
 
         public override void OnReturn()
